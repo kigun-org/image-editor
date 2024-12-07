cb3f37fcce570e5803dfaf5c422755271db9598e
Mihai Tarce <mihai@kigun.org>
2024-12-07 10:26:20 +0800

    Changed CSS outfile name to match JS.

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
             ],
             formats: ['es'],
             fileName: (_, entryAlias) => `${entryAlias}.js`,
+            cssFileName: 'ImageEditor'
         }
     }
 })
