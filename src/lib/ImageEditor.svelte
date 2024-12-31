dcda74faff430e1425f08b1d2f6fd8f7253ce203
Mihai Tarce <mihai@kigun.org>
2024-12-31 21:42:58 +0800

    Cleaned up image editor panel layout.

--- src/lib/ImageEditor.svelte
+++ src/lib/ImageEditor.svelte
@@ -109,42 +109,41 @@
                     k-bg-base-200 k-transition-colors"
              ondragover={handleDragOver} ondrop={handleDrop} role="form">
 
-            <label class="k-flex-1 k-flex k-items-center k-justify-center k-gap-8
-                          hover:k-bg-base-300 k-transition-colors k-cursor-pointer k-py-20">
+            <label class="k-flex-1 k-flex k-items-center k-justify-center k-gap-6
+                          hover:k-bg-base-300 k-transition-colors k-cursor-pointer k-px-4 k-py-16">
                 <input bind:this={fileInputElement} type="file" accept="image/*" class="k-hidden"
                        onchange={handleInputChange}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
+                <svg class="k-flex-shrink-0" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"/>
                     <path d="M7 9l5 -5l5 5"/>
                     <path d="M12 4l0 12"/>
                 </svg>
-                <span role="button" tabindex="0" bind:this={fileInputLabelElement}
+                <span role="button" tabindex="0" class="k-text-lg" bind:this={fileInputLabelElement}
                       onkeyup={(e) => { if (e.key === "Enter" || e.key === " ") fileInputElement.click() }}>
-                        Upload an image by clicking here,<br>
-                        or by dragging and dropping
+                        Upload an image by clicking here, or by dragging and dropping
                     </span>
             </label>
 
-            <div class="k-flex-1 k-flex k-items-center k-justify-center k-gap-8
-                        hover:k-bg-base-300 k-transition-colors k-py-20">
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
+            <div class="k-flex-1 k-flex k-items-center k-justify-center k-gap-6
+                        hover:k-bg-base-300 k-transition-colors k-px-4 k-py-16">
+                <svg class="k-flex-shrink-0" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"/>
                     <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"/>
                 </svg>
-                <span>Paste an image (ctrl/⌘ + v)</span>
+                <span class="k-text-lg">Paste an image (ctrl/⌘ + v)</span>
             </div>
 
             {#if galleryURL !== undefined}
-                <button class="k-flex-1 k-flex k-items-center k-justify-center k-gap-8
-                            hover:k-bg-base-300 k-transition-colors k-py-20"
+                <button class="k-flex-1 k-flex k-items-center k-justify-center k-gap-6
+                            hover:k-bg-base-300 k-transition-colors k-px-4 k-py-16"
                         aria-label="Select an existing image" onclick={() => showBrowser = true}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
+                    <svg class="k-flex-shrink-0" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M6.657 18c-2.572 0 -4.657 -2.007 -4.657 -4.483c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 1.927 -1.551 3.487 -3.465 3.487h-11.878"/>
                     </svg>
-                    <span>Select an existing image</span>
+                    <span class="k-text-lg">Select an existing image</span>
                 </button>
             {/if}
 
